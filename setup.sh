ffb275eb1e1e5a4b17947f2e4b08d86cedbaca81
zenobit <zenobit@disroot.org>
2025-05-23 02:46:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,44 +13,15 @@ done
 cd $CURRENT
 
 sudo bash <<'SCRIPT'
-# Install dotbins
-pip install dotbins
-
-dotbins sync
+# Install latest static zellij
+curl -L "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz" |
+  tar -C /usr/bin -xzf - zellij
 
 # Install gitpod.zellij plugin
 plugin_path="/usr/bin/gitpod_zellij"
 curl -L "https://github.com/axonasif/gitpod.zellij/releases/latest/download/gitpod_zellij-x86_64-unknown-linux-musl.tar.gz" |
   tar -C /usr/bin -xzf - gitpod_zellij
 SCRIPT
-
-cat >>"$HOME/.bashrc" <<'SNIP'
-_os=$(uname -s | tr '[:upper:]' '[:lower:]')
-[[ "$_os" == "darwin" ]] && _os="macos"
-
-_arch=$(uname -m)
-[[ "$_arch" == "x86_64" ]] && _arch="amd64"
-[[ "$_arch" == "aarch64" || "$_arch" == "arm64" ]] && _arch="arm64"
-
-export PATH="$_os/$_arch/bin:$PATH"
-
-# Tool-specific configurations
-# Configuration for fzf
-if command -v fzf >/dev/null 2>&1; then
-    eval "$(fzf --bash)"
-fi
-
-# Configuration for lazygit
-if command -v lazygit >/dev/null 2>&1; then
-    alias lg="lazygit"
-fi
-
-# Configuration for starship
-if command -v starship >/dev/null 2>&1; then
-    eval "$(starship init bash)"
-fi
-
-SNIP
 
 # Auto start zellij on SSH or xtermjs
 cat >>"$HOME/.bashrc" <<'SNIP'
@@ -61,6 +32,16 @@ if ! test -v ZELLIJ && ! pgrep -f "$HOME/.vscode-server/bin" 1>/dev/null && (tes
   } fi
     exec zellij attach
 } fi
+SNIP
+
+#sudo bash <<'SCRIPT'
+# Install dotbins
+#pip install dotbins
+
+#dotbins sync
+
+cat >>"$HOME/.bashrc" <<'SNIP'
+source shell/bash.sh
 SNIP
 
 rm -rf $TMPDIR
