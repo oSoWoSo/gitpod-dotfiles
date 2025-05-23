7f50d037eac15ea2b7e3b97a36d3439277deb78e
zenobit <zenobit@disroot.org>
2025-05-23 02:26:47 +0200

    Source bash.sh

--- setup.sh
+++ setup.sh
@@ -24,6 +24,10 @@ curl -L "https://github.com/axonasif/gitpod.zellij/releases/latest/download/gitp
   tar -C /usr/bin -xzf - gitpod_zellij
 SCRIPT
 
+cat >>"$HOME/.bashrc" <<'SNIP'
+source shell/bash.sh
+SNIP
+
 # Auto start zellij on SSH or xtermjs
 cat >>"$HOME/.bashrc" <<'SNIP'
 if ! test -v ZELLIJ && ! pgrep -f "$HOME/.vscode-server/bin" 1>/dev/null && (test -v SSH_CONNECTION || test "$PPID" == "$(pgrep -f '/ide/xterm/bin/node /ide/xterm/index.cjs' | head -n1)"); then {
